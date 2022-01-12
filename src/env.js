588f87f190acae5f8f952fed5dd39e1c627134ad
quanton314 <harshavardhan32952@gmail.com>
2022-01-12 18:02:17 +0530

    .

--- src/env.js
+++ src/env.js
@@ -1,15 +1,15 @@
-import dotenv from 'dotenv';
+// import dotenv from 'dotenv';
 
-dotenv.config();
+// dotenv.config();
 
-export let API_URL = process.env.API_URL || 'https://192.168.1.8:8000';
+// export let API_URL = process.env.API_URL || 'https://192.168.1.8:8000';
 
-export let REDIRECT_URL =
-    process.env.REDIRECT_URL || 'https://192.168.1.8:5499';
+// export let REDIRECT_URL =
+//     process.env.REDIRECT_URL || 'https://192.168.1.8:5499';
 
-let urls = { API_URL, REDIRECT_URL };
+// let urls = { API_URL, REDIRECT_URL };
 
-export default urls;
+// export default urls;
 
-// exports.API_URL = 'https://mern-short-api.herokuapp.com';
-// exports.REDIRECT_URL = 'https://url-bits.herokuapp.com';
+exports.API_URL = 'https://mern-short-api.herokuapp.com';
+exports.REDIRECT_URL = 'https://url-bits.herokuapp.com';
