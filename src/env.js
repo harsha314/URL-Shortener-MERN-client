8b6f2bc2ff3cb08885132d8aa582744c6a815101
quanton314 <harshavardhan32952@gmail.com>
2022-01-12 22:46:59 +0530

    .

--- src/env.js
+++ src/env.js
@@ -2,13 +2,9 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-// export let API_URL = process.env.REACT_APP_API_URL;
+export let API_URL = process.env.REACT_APP_API_URL;
 
-// export let REDIRECT_URL = process.env.REACT_APP_REDIRECT_URL;
-
-export let API_URL = 'https://mern-short-api.herokuapp.com';
-
-export let REDIRECT_URL = 'https://mern-short-api.herokuapp.com';
+export let REDIRECT_URL = process.env.REACT_APP_REDIRECT_URL;
 
 let urls = { API_URL, REDIRECT_URL };
 
